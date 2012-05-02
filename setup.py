d1ceee486efe673318f436f3ebc1bc5cbfc66931
Adam Lindsay <atl@alum.mit.edu>
2012-05-02 06:29:35 -0700

    try versioneer for release management

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+import versioneer
+
+versioneer.versionfile_source = 'smartdc/_version.py'
+versioneer.versionfile_build = 'smartdc/_version.py'
+versioneer.tag_prefix = 'v' # tags are like 1.2.0
+versioneer.parentdir_prefix = 'smartdc-' # dirname like 'myproject-1.2.0'
 
 with open('README.rst') as file:
     long_description = file.read()
@@ -7,7 +13,8 @@ with open('CHANGES.rst') as file:
 
 setup(
     name='smartdc',
-    version='0.1.7',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description="Joyent SmartDataCenter CloudAPI connector using http-signature authentication via Requests",
     long_description=long_description,
     classifiers=[
--- smartdc/__init__.py
+++ smartdc/__init__.py
@@ -1,2 +1,6 @@
 from .datacenter import *
 from .machine import *
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
